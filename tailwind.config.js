b5af65cb356fc22d7f2a473e688277783a419966
JamissonSilvaTico <jamissondasilvatico@gmail.com>
2025-09-25 01:05:58 -0400

    ajustes frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./*.{js,ts,jsx,tsx}",
+    "./{components,context,hooks,pages}/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
